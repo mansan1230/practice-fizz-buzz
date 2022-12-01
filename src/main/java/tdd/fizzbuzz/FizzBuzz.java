1c452df19f33fd1c391f5737a51c8b5cc2d7e420
VINCENT TAM (SYS-TSV-ISD-OOCLL/HKG) <TAMVI@OOCL.COM>
2022-12-01 15:08:29 +0800

    teat 1. 3. 5.7. 35.37.57

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -1,4 +1,21 @@
 package tdd.fizzbuzz;
 
 public class FizzBuzz {
+
+    public String count(int seq)
+    {
+        if (seq % 5 == 0 && seq % 3 == 0)
+            return String.valueOf("FizzBuzz");
+        else if (seq % 3 == 0 && seq % 7 == 0)
+            return String.valueOf("WhizzFizz");
+        else if (seq % 7 == 0 && seq % 5 == 0)
+            return String.valueOf("WhizzBuzz");
+        else if (seq % 5 == 0)
+            return String.valueOf("Buzz");
+        else if (seq % 7 == 0)
+            return String.valueOf("Whizz");
+        else if (seq % 3 == 0)
+            return String.valueOf("Fizz");
+        return String.valueOf(seq);
+    }
 }
--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,77 @@
 package tdd.fizzbuzz;
 
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+
+
+    @Test
+    void should_get_number_when_count_given_one()
+    {
+        int seq = 1;
+
+        String expect = new FizzBuzz().count(seq);
+
+        assertEquals("1", expect);
+    }
+    @Test
+    void should_get_number_when_count_is_three_multiple()
+    {
+        int seq = 3;
+
+        String expect = new FizzBuzz().count(seq);
+
+        assertEquals("Fizz", expect);
+    }
+    @Test
+    void should_get_number_when_count_is_five_multiple()
+    {
+        int seq = 5;
+
+        String expect = new FizzBuzz().count(seq);
+
+        assertEquals("Buzz", expect);
+    }
+
+    @Test
+    void should_get_number_when_count_is_seven_multiple()
+    {
+        int seq = 7;
+
+        String expect = new FizzBuzz().count(seq);
+
+        assertEquals("Whizz", expect);
+    }
+    @Test
+    void should_get_number_when_count_is_seven_and_five_multiple()
+    {
+        int seq = 15;
+
+        String expect = new FizzBuzz().count(seq);
+
+        assertEquals("FizzBuzz", expect);
+    }
+
+    @Test
+    void should_get_number_when_count_is_three_and_seven_multiple()
+    {
+        int seq = 21;
+
+        String expect = new FizzBuzz().count(seq);
+
+        assertEquals("WhizzFizz", expect);
+    }
+
+    @Test
+    void should_get_number_when_count_is_five_and_seven_multiple()
+    {
+        int seq = 35;
+
+        String expect = new FizzBuzz().count(seq);
+
+        assertEquals("WhizzBuzz", expect);
+    }
 }
